9e8399001e70b105546f8f87a4f8180910081e4e
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-19 00:09:30 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Layout/Main';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import SignUp from './Components/SignUp/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signUp',
+        element: <SignUp />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
